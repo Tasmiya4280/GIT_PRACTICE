bb1ffc0f9e3f251bd7e6110595680a0487593ee8
Tasmiya4280 <tasmiyairfan4280@gmail.com>
2025-01-24 17:14:23 +0500

    9th commit of  code

--- demo-wrong.py
+++ demo-wrong.py
@@ -1,9 +1,6 @@
-"""
-This module contains examples of incorrect code formatting and their corrections.
-"""
-
 def hello():
     """
     Returns a greeting message: 'hello world'.
     """
     return "hello world"
+

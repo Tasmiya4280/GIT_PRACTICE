976038f05ce0f7b14dcf1bc1b40444d69416196c
Tasmiya4280 <tasmiyairfan4280@gmail.com>
2025-01-24 16:57:41 +0500

    8th commit of  code

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-"""
-This module demonstrates a simple 'hello world' program.
-"""
-
-print("hello world!")
-
-# Ensure there is a blank line here (final newline)
+def hello():
+    """
+    practice git
+    """
+    return "hello world"
